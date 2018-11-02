b3dd41c34683b5dc40e6f265f73b42fbda59e376
Joseph <joseph.vavala@epfl.ch>
2018-11-02 16:15:32 +0100

    initial commit

---
+++ BitApp/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using NBitcoin;
+using QBitNinja.Client;
+
+namespace BitApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var  privateKey = new Key();
+            var testNetPrivateKey = privateKey.GetBitcoinSecret(Network.TestNet);
+
+            Console.WriteLine(testNetPrivateKey);
+            var publicKey = privateKey.PubKey; // compute public key
+            var address = publicKey.GetAddress(Network.TestNet);
+
+            Console.WriteLine("pubkey: \t" + publicKey);
+            Console.WriteLine("address: \t" + address);
+        }
+    }
+}
