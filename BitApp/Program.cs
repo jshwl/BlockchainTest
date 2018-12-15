95e6a5a13478342b490b9c79bc0ae6113ca1149d
Joseph <joseph.vavala@epfl.ch>
2018-12-15 23:13:59 +0100

    hex to ascii

--- BitApp/Program.cs
+++ BitApp/Program.cs
@@ -6,38 +6,36 @@ namespace BitApp
 {
     class Program
     {
+        public static string ConvertHex(String hexString)
+        {
+            try
+            {
+                string ascii = string.Empty;
+
+                for (int i = 0; i < hexString.Length; i += 2)
+                {
+                    String hs = string.Empty;
+
+                    hs   = hexString.Substring(i,2);
+                    uint decval =   System.Convert.ToUInt32(hs, 16);
+                    char character = System.Convert.ToChar(decval);
+                    ascii += character;
+
+                }
+
+                return ascii;
+            }
+            catch (Exception ex) { Console.WriteLine(ex.Message); }
+
+            return string.Empty;
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Start program");
-            Key privateKey;
-            string vanity = "jose";
-            while(true){
-                privateKey = new Key();
-                var publicKey = privateKey.PubKey; // compute public key
-                var testNetPrivateKey = privateKey.GetBitcoinSecret(Network.TestNet);
-                var address = publicKey.GetAddress(Network.TestNet);
-                string v_address = address.ToString().Substring(1,5);
-                Console.WriteLine("VanityAddress: \t" + v_address + "\t address:\t" + address);
-                if(vanity.Equals(v_address.ToLower())){
-                    Console.WriteLine("public key : \t" + publicKey);
-                    Console.WriteLine("TestNet private key: \t" + testNetPrivateKey);
-                    Console.WriteLine("Vanityzaddress:\t" + address);
-                    break;
-                }
-            }
-            
-            //var testNetPrivateKey = privateKey.GetBitcoinSecret(Network.TestNet);
-
-            //Console.WriteLine(testNetPrivateKey);
-            
-            
-
-                            //Console.WriteLine("TestNet private key: \t" + testNetPrivateKey);
-                //Console.WriteLine("Public Key : \t" + publicKey);
-                //Console.WriteLine("pkstring: \t" + pkstring.ToLower();
-
-            
-            Console.WriteLine("end\n");
+            String hex1 = "4a5e1e4baab89f3a32518a88c31bc87f618f76673e2cc77ab2127b7afdeda33b";
+            String hex = "04ffff001d0104455468652054696d65732030332f4a616e2f32303039204368616e63656c6c6f72206f6e206272696e6b206f66207365636f6e64206261696c6f757420666f722062616e6b73";
+            string ascii = ConvertHex(hex);
+            Console.WriteLine(ascii + "\n End");
         }
     }
 }
