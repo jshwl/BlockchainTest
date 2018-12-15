88079c5c96b7df830893cdb362b3defc90a3339b
Anwar Ahmad Quraishi <anwar.quraishi@epfl.ch>
2018-12-15 17:57:37 +0100

    Merged code

--- BitApp/Program.cs
+++ BitApp/Program.cs
@@ -10,7 +10,39 @@ namespace BitApp
         static void Main(string[] args)
         {
 
-            // ##################################################################
+            Console.WriteLine("Start program");
+            Key privateKey;
+            string vanity = "jose";
+            while(true){
+                privateKey = new Key();
+                var publicKey = privateKey.PubKey; // compute public key
+                var testNetPrivateKey = privateKey.GetBitcoinSecret(Network.TestNet);
+                var address = publicKey.GetAddress(Network.TestNet);
+                string v_address = address.ToString().Substring(1,5);
+                Console.WriteLine("VanityAddress: \t" + v_address + "\t address:\t" + address);
+                if(vanity.Equals(v_address.ToLower())){
+                    Console.WriteLine("public key : \t" + publicKey);
+                    Console.WriteLine("TestNet private key: \t" + testNetPrivateKey);
+                    Console.WriteLine("Vanityzaddress:\t" + address);
+                    break;
+                }
+            }
+            
+            //var testNetPrivateKey = privateKey.GetBitcoinSecret(Network.TestNet);
+
+            //Console.WriteLine(testNetPrivateKey);
+            
+            
+
+                            //Console.WriteLine("TestNet private key: \t" + testNetPrivateKey);
+                //Console.WriteLine("Public Key : \t" + publicKey);
+                //Console.WriteLine("pkstring: \t" + pkstring.ToLower();
+
+            
+            Console.WriteLine("end\n");
+
+            // UNCOMMENT THE FOLLOWING BEFORE SUBMISSION
+           /* // ##################################################################
             // ### Wallet management in C#
             // ##################################################################
             generateAndSaveKeys();
@@ -37,7 +69,7 @@ namespace BitApp
             // ##################################################################
             sendMoney();
 
-            monitorBlockConfirmations("ccc50aafe5b106f9ef7942e226a16b16614f25dee2c54f97d0a326626381d319", 20.0);
+            monitorBlockConfirmations("ccc50aafe5b106f9ef7942e226a16b16614f25dee2c54f97d0a326626381d319", 20.0);*/
             
             
             
